9e463bd95b59ae5cad76cf3b727d3e8e91720bb8
mddengo <mddengo@gmail.com>
2016-02-06 01:14:39 -0500

    basic html

--- purify/app.js
+++ purify/app.js
@@ -14,6 +14,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// URL's for views
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, './views', 'index.html'));
+});
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
